15c49fb4f984001c73f7237548c14634a47b5461
Fedor <fedor@dps.uibk.ac.at>
2021-02-07 06:55:13 +0100

    spec provision implemented

--- src/main/java/at/uibk/dps/ee/io/spec/SpecificationProviderFile.java
+++ src/main/java/at/uibk/dps/ee/io/spec/SpecificationProviderFile.java
@@ -1,17 +1,31 @@
 package at.uibk.dps.ee.io.spec;
 
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import com.google.inject.Inject;
 
+import at.uibk.dps.ee.io.json.ResourceEntry;
+import at.uibk.dps.ee.io.json.ResourceInformationJsonFile;
+import at.uibk.dps.ee.model.constants.ConstantsEEModel;
 import at.uibk.dps.ee.model.graph.EnactmentGraph;
 import at.uibk.dps.ee.model.graph.EnactmentGraphProvider;
 import at.uibk.dps.ee.model.graph.EnactmentSpecification;
 import at.uibk.dps.ee.model.graph.ResourceGraph;
 import at.uibk.dps.ee.model.graph.ResourceGraphProvider;
 import at.uibk.dps.ee.model.graph.SpecificationProvider;
+import at.uibk.dps.ee.model.properties.PropertyServiceFunction;
+import at.uibk.dps.ee.model.properties.PropertyServiceFunctionUser;
+import at.uibk.dps.ee.model.properties.PropertyServiceMapping;
+import at.uibk.dps.ee.model.properties.PropertyServiceResource.ResourceType;
+import at.uibk.dps.ee.model.properties.PropertyServiceResourceServerless;
+import at.uibk.dps.ee.model.properties.PropertyServiceFunction.UsageType;
+import net.sf.opendse.model.Mapping;
 import net.sf.opendse.model.Mappings;
 import net.sf.opendse.model.Resource;
-import net.sf.opendse.model.Specification;
 import net.sf.opendse.model.Task;
+import net.sf.opendse.model.properties.TaskPropertyService;
 
 /**
  * The {@link SpecificationProviderFile} creates the specification by taking the
@@ -53,17 +67,68 @@ public class SpecificationProviderFile implements SpecificationProvider {
 	}
 
 	/**
-	 * Reads the json file with the file information and uses it to create the mappings.
+	 * Reads the json file with the file information and uses it to create the
+	 * mappings.
 	 * 
-	 * @param eGraph the enactment graph
-	 * @param rGraph the resource graph
+	 * @param eGraph   the enactment graph
+	 * @param rGraph   the resource graph
 	 * @param filePath the file path to the resource information
 	 * @return the mappings connected the eGraph and the rGraph
 	 */
-	protected Mappings<Task, Resource> createMappings(EnactmentGraph eGraph, ResourceGraph rGraph, String filePath){
-		
+	protected Mappings<Task, Resource> createMappings(EnactmentGraph eGraph, ResourceGraph rGraph, String filePath) {
+		Mappings<Task, Resource> result = new Mappings<>();
+		ResourceInformationJsonFile resInfo = ResourceInformationJsonFile.readFromFile(filePath);
+		eGraph.getVertices().stream().filter(task -> TaskPropertyService.isProcess(task))
+				.filter(task -> PropertyServiceFunction.getUsageType(task).equals(UsageType.User))
+				.flatMap(task -> getMappingsForTask(task, resInfo, rGraph).stream())
+				.forEach(mapping -> result.add(mapping));
+		return result;
 	}
-	
+
+	/**
+	 * Creates the mappings for the provided task based on the given resource
+	 * information.
+	 * 
+	 * @param task    the provided task
+	 * @param resInfo the given resource information.
+	 * @param rGraph  the resource graph
+	 * @return the mappings for the provided task based on the given resource
+	 *         information
+	 */
+	protected Set<Mapping<Task, Resource>> getMappingsForTask(Task task, ResourceInformationJsonFile resInfo,
+			ResourceGraph rGraph) {
+		String funcTypeString = PropertyServiceFunctionUser.getFunctionType(task);
+		return resInfo.stream().filter(functionEntry -> funcTypeString.equals(functionEntry.getFunctionType()))
+				.flatMap(functionEntry -> functionEntry.getResources().stream())
+				.map(resEntry -> getResourceForResourceEntry(rGraph, resEntry))
+				.map(res -> PropertyServiceMapping.createMapping(task, res)).collect(Collectors.toSet());
+	}
+
+	/**
+	 * Gets the resource node matching the provided resource entry
+	 * 
+	 * @param rGraph   the resource graph
+	 * @param resEntry the resource entry
+	 * @return the resource node matching the provided resource entry
+	 */
+	protected Resource getResourceForResourceEntry(ResourceGraph rGraph, ResourceEntry resEntry) {
+		Optional<Resource> result;
+		if (resEntry.getType().equals(ResourceType.Local.name())) {
+			// Resource is local EE
+			result = Optional.ofNullable(rGraph.getVertex(ConstantsEEModel.idLocalResource));
+		} else if (resEntry.getType().equals(ResourceType.Serverless.name())) {
+			// Serverless resource => look for the Uri
+			if (!resEntry.getProperties().containsKey(PropertyServiceResourceServerless.propNameUri)) {
+				throw new IllegalArgumentException("No Uri annotated for serverless resource");
+			}
+			String uri = resEntry.getProperties().get(PropertyServiceResourceServerless.propNameUri).getAsString();
+			result = Optional.ofNullable(rGraph.getVertex(uri));
+		} else {
+			throw new IllegalArgumentException("Unknown resource type: " + resEntry.getType());
+		}
+		return result.orElseThrow();
+	}
+
 	@Override
 	public EnactmentSpecification getSpecification() {
 		return specification;
--- src/test/java/at/uibk/dps/ee/io/spec/SpecificationProviderFileTest.java
+++ src/test/java/at/uibk/dps/ee/io/spec/SpecificationProviderFileTest.java
@@ -4,13 +4,40 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import at.uibk.dps.ee.io.resources.ResourceGraphProviderFile;
+import at.uibk.dps.ee.io.testconstants.ConstantsTestCoreEEiO;
+import at.uibk.dps.ee.model.graph.EnactmentGraph;
+import at.uibk.dps.ee.model.graph.EnactmentGraphProvider;
+import at.uibk.dps.ee.model.graph.ResourceGraphProvider;
+import at.uibk.dps.ee.model.properties.PropertyServiceFunctionUser;
+import net.sf.opendse.model.Mappings;
+import net.sf.opendse.model.Resource;
+import net.sf.opendse.model.Task;
+
+import static org.mockito.Mockito.mock;
+
+import static org.mockito.Mockito.when;
+
 public class SpecificationProviderFileTest {
 
 	@Test
 	public void testMappingCreation() {
+		EnactmentGraph eGraph = new EnactmentGraph();
+		Task t1 = PropertyServiceFunctionUser.createUserTask("t1", "addition");
+		Task t2 = PropertyServiceFunctionUser.createUserTask("t2", "subtraction");
+		eGraph.addVertex(t1);
+		eGraph.addVertex(t2);
+		EnactmentGraphProvider eProvider = mock(EnactmentGraphProvider.class);
+		when(eProvider.getEnactmentGraph()).thenReturn(eGraph);
 		
+		String filePath = ConstantsTestCoreEEiO.resourceTestInputPath;
+		ResourceGraphProvider rProvider = new ResourceGraphProviderFile(filePath);
+		SpecificationProviderFile tested = new SpecificationProviderFile(eProvider, rProvider, filePath);
 		
+		Mappings<Task, Resource> result = tested.getMappings();
 		
+		assertEquals(4, result.size());
+		assertEquals(2, result.get(t1).size());
+		assertEquals(2, result.get(t2).size());
 	}
-
 }
